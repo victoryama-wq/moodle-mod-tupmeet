f932f156484a452282ba10e08d95e5913c959c08
victoryama-wq <victor.yama@tecplayacar.edu.mx>
2026-09-14 16:59:17 -0500

    lang: add Spanish strings

---
+++ lang/es/tupmeet.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+$string['pluginname'] = 'TUP Meet';
+$string['modulename'] = 'TUP Meet';
+$string['modulenameplural'] = 'TUP Meet';
+$string['pluginadministration'] = 'Administración de TUP Meet';
+$string['tupmeet:addinstance'] = 'Añadir una actividad TUP Meet';
+$string['tupmeet:view'] = 'Ver una actividad TUP Meet';
+$string['tupmeet:manage'] = 'Administrar una actividad TUP Meet';
+$string['meetingname'] = 'Nombre de la reunión';
+$string['scheduling'] = 'Programación';
+$string['startdatetime'] = 'Fecha y hora de inicio';
+$string['enddatetime'] = 'Fecha y hora de finalización';
+$string['isrecurring'] = 'Reunión recurrente';
+$string['recurrenceinterval'] = 'Repetir cada (semanas)';
+$string['recurrencedays'] = 'Días de repetición';
+$string['recurrenceuntil'] = 'Repetir hasta';
+$string['meetsettings'] = 'Google Meet';
+$string['autorecord'] = 'Grabar automáticamente';
+$string['autotranscript'] = 'Generar transcripción automáticamente';
+$string['publicationmode'] = 'Publicación de grabaciones';
+$string['publicationmanual'] = 'Requiere aprobación del docente';
+$string['publicationautomatic'] = 'Publicar automáticamente';
+$string['errormissingname'] = 'Indique un nombre para la reunión.';
+$string['errorendbeforestart'] = 'La fecha/hora de finalización debe ser posterior al inicio.';
+$string['errorrecurrenceinterval'] = 'El intervalo de repetición debe ser de al menos una semana.';
+$string['errorrecurrenceday'] = 'Seleccione al menos un día para la reunión recurrente.';
+$string['errorrecurrenceuntil'] = 'La fecha final de recurrencia no puede ser anterior al inicio.';
+$string['phase0notice'] = 'Fase 0 instalada. La conexión con la cuenta maestra de Google y la generación automática del enlace Meet se habilitarán en la siguiente fase.';
+$string['privacy:metadata'] = 'La Fase 0 de TUP Meet no almacena datos personales asociados directamente a usuarios de Moodle.';
