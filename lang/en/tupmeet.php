d00f6c0f815ddc7b115e5d5a8aa818dedd340507
victoryama-wq <victor.yama@tecplayacar.edu.mx>
2026-09-14 16:59:07 -0500

    lang: add English strings

---
+++ lang/en/tupmeet.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+$string['pluginname'] = 'TUP Meet';
+$string['modulename'] = 'TUP Meet';
+$string['modulenameplural'] = 'TUP Meet';
+$string['pluginadministration'] = 'TUP Meet administration';
+$string['tupmeet:addinstance'] = 'Add a TUP Meet activity';
+$string['tupmeet:view'] = 'View a TUP Meet activity';
+$string['tupmeet:manage'] = 'Manage a TUP Meet activity';
+$string['meetingname'] = 'Meeting name';
+$string['scheduling'] = 'Scheduling';
+$string['startdatetime'] = 'Start date and time';
+$string['enddatetime'] = 'End date and time';
+$string['isrecurring'] = 'Recurring meeting';
+$string['recurrenceinterval'] = 'Repeat every (weeks)';
+$string['recurrencedays'] = 'Repeat on';
+$string['recurrenceuntil'] = 'Repeat until';
+$string['meetsettings'] = 'Google Meet';
+$string['autorecord'] = 'Record automatically';
+$string['autotranscript'] = 'Generate transcript automatically';
+$string['publicationmode'] = 'Recording publication';
+$string['publicationmanual'] = 'Teacher approval required';
+$string['publicationautomatic'] = 'Publish automatically';
+$string['errorendbeforestart'] = 'The end date/time must be after the start date/time.';
+$string['errorrecurrenceinterval'] = 'The recurrence interval must be at least one week.';
+$string['errorrecurrenceday'] = 'Select at least one day for the recurring meeting.';
+$string['errorrecurrenceuntil'] = 'The recurrence end date cannot be earlier than the meeting start.';
+$string['phase0notice'] = 'Phase 0 is installed. Google master-account connection and automatic Meet link generation will be enabled in the next phase.';
+$string['privacy:metadata'] = 'TUP Meet Phase 0 does not store personal data directly associated with Moodle users.';
